b9a0964d7ee89a1ffa44d138e313d1ae3b955a3c
repositoryVPS <vpstrader93@gmail.com>
2024-06-24 16:55:35 -0300

    att Game Manager - spawn

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Core.Singleton;
+using DG.Tweening;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
@@ -10,5 +11,32 @@ public class GameManager : Singleton<GameManager>
 
     [Header("Enemies")]
     public List<GameObject> enemies;
+
+    [Header("References")]
+    public Transform startPoint;
+    private GameObject _currentPlayer;
+
+
+    [Header("Animation")]
+    public float animationDuration = 0.2f;
+    public float delay = .05f;
+    public Ease ease = Ease.OutBack;
+
+
+    private void Start()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+        SpawnPlayer();
+    }
+    private void SpawnPlayer()
+    {
+        _currentPlayer = Instantiate(player);
+        _currentPlayer.transform.position = startPoint.position;
+        _currentPlayer.transform.DOScale(0,animationDuration).SetEase(ease).From();
+    }
 
 }
