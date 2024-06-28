cf170a826b198bd319a22cba53a1f5782a8dd241
repositoryVPS <vpstrader93@gmail.com>
2024-06-28 20:01:01 -0300

    add animation and swipe

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,7 +9,7 @@ using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigibody;
-    
+
     [Header("Speed Setup")]
     public Vector2 friction = new Vector2(.1f, 0);
     public float speed;
@@ -20,30 +20,52 @@ public class Player : MonoBehaviour
     [Header("Animation Setup")]
     public float JumpScaleY = 1.5f;
     public float JumpScaleX = .7f;
-    public float animationDuration = .3f;
+    public float animationDuration = .1f;
     private float _currentSpeed;
-    private void Update()
-    {
-        HandleJump();
-        HandleMoviment();
-    }
 
+
+    [Header("Animation Player")]
+    public string triggerRun = "Run";
+    public Animator animator;
+    public float swipeDuration = .1f;
     private void HandleMoviment()
     {
-        
-        if(Input.GetKey(KeyCode.LeftControl))
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
             _currentSpeed = speedRun;
+            animator.speed = 2;
+        }
         else
+        {
             _currentSpeed = speed;
-        
+            animator.speed = 1;
+        }
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+            animator.SetBool(triggerRun, true);
             myRigibody.velocity = new Vector2(-_currentSpeed, myRigibody.velocity.y);
+            if(myRigibody.transform.localScale.x != -1)
+            {
+                myRigibody.transform.DOScaleX(-1,swipeDuration);
+            }
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
+            animator.SetBool(triggerRun, true);
             myRigibody.velocity = new Vector2(_currentSpeed, myRigibody.velocity.y);
+            if(myRigibody.transform.localScale.x != -1)
+            {
+                myRigibody.transform.DOScaleX(1,swipeDuration);
+            }
         }
+        else
+        {
+            animator.SetBool(triggerRun, false);
+        }
+
+
 
         if (myRigibody.velocity.x > 0)
             myRigibody.velocity -= friction;
@@ -51,21 +73,25 @@ public class Player : MonoBehaviour
             myRigibody.velocity += friction;
     }
 
-
+    private void Update()
+    {
+        HandleJump();
+        HandleMoviment();
+    }
     private void HandleJump()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             myRigibody.velocity = Vector2.up * forceJump;
             myRigibody.transform.localScale = Vector2.one;
-            DOTween.Kill(myRigibody.transform);
             HandleScaleJump();
+            DOTween.Kill(myRigibody.transform);
         }
     }
     private void HandleScaleJump()
     {
-        myRigibody.transform.DOScaleY(JumpScaleY,animationDuration).SetLoops(2,LoopType.Yoyo).SetEase(ease);
-        myRigibody.transform.DOScaleX(JumpScaleX,animationDuration).SetLoops(2,LoopType.Yoyo).SetEase(ease);
+        myRigibody.transform.DOScaleY(JumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        myRigibody.transform.DOScaleX(JumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
     }
 }
 
